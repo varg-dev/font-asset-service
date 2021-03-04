77c7331a0a4d43ed402e34a43538cb11a18425a3
Willy Scheibel <willy.scheibel@hpi.de>
2021-03-04 23:14:09 +0100

    Start assets API

--- service/v1/__init__.py
+++ service/v1/__init__.py
@@ -117,8 +117,8 @@ async def api_head_font(identifier: str, response: Response):
     response.status_code = status.HTTP_200_OK
 
 
-@router.head("/fonts/{identifier}", tags=["fonts"])
-async def api_head_font(identifier: str, response: Response):
+@router.get("/fonts/{identifier}", tags=["fonts"])
+async def api_get_font(identifier: str, response: Response):
 
     path = os.path.join(fonts_dir(), identifier)
 
@@ -129,12 +129,179 @@ async def api_head_font(identifier: str, response: Response):
     response.status_code = status.HTTP_200_OK
 
     filename, extension = os.path.splitext(os.path.basename(path))
+
+    result = []
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            basename = os.path.basename(entry.path)
+            result.append({
+                'identifier': basename,
+                'url': f'/fonts/{identifier}/{basename}'
+            })
+    
+    response.status_code = status.HTTP_200_OK
     
     return {
         'identifier': filename+extension,
         'format': extension,
-        'url': f'/fonts/{filename+extension}'
+        'assets': result,
+        'url': f'/fonts/{identifier}'
     }
 
 
 # ASSETS
+
+@router.head("/fonts/{identifier}/{asset_parameter_hash}", tags=["assets"])
+async def api_head_font_asset(identifier: str, asset_parameter_hash: str, response: Response):
+
+    path = os.path.join(fonts_dir(), identifier)
+
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, asset_parameter_hash)
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, 'metainfo.json')
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    metainfo = {}
+    with open(path, 'r') as f:
+        metainfo = json.load(f)
+    
+    if not 'assets' in metainfo:
+        response.status_code = status.HTTP_409_CONFLICT
+        return {}
+    
+    response.status_code = status.HTTP_200_OK
+
+
+@router.get("/fonts/{identifier}/{asset_parameter_hash}", tags=["assets"])
+async def api_get_font_asset(identifier: str, asset_parameter_hash: str, response: Response):
+
+    path = os.path.join(fonts_dir(), identifier)
+
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, asset_parameter_hash)
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, 'metainfo.json')
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    metainfo = {}
+    with open(path, 'r') as f:
+        metainfo = json.load(f)
+    
+    if not 'assets' in metainfo:
+        response.status_code = status.HTTP_409_CONFLICT
+        return {}
+    
+    assets_info = metainfo['assets']
+    
+    response.status_code = status.HTTP_200_OK
+
+    result = []
+
+    for asset_type in assets_info:
+        asset_info = assets_info[asset_type]
+        result.append({
+            'type': asset_type,
+            'arguments': asset_info['arguments'],
+            'url': f'/fonts/{identifier}/{asset_parameter_hash}/{asset_type}',
+        })
+
+    return result
+
+
+# ASSETS DOWNLOAD
+
+@router.head("/fonts/{identifier}/{asset_parameter_hash}/{asset_type}", tags=["assets"])
+async def api_head_font_asset_download(identifier: str, asset_parameter_hash: str, asset_type: str, response: Response):
+
+    path = os.path.join(fonts_dir(), identifier)
+
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, asset_parameter_hash)
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, 'metainfo.json')
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    metainfo = {}
+    with open(path, 'r') as f:
+        metainfo = json.load(f)
+    
+    if not 'assets' in metainfo:
+        response.status_code = status.HTTP_409_CONFLICT
+        return {}
+    
+    if not asset_type in metainfo['assets']:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    response.status_code = status.HTTP_200_OK
+
+
+@router.get("/fonts/{identifier}/{asset_parameter_hash}/{asset_type}", tags=["assets"])
+async def api_get_font_asset_download(identifier: str, asset_parameter_hash: str, asset_type: str, response: Response):
+
+    path = os.path.join(fonts_dir(), identifier)
+
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, asset_parameter_hash)
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    path = os.path.join(path, 'metainfo.json')
+    
+    if not os.path.exists(path):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    metainfo = {}
+    with open(path, 'r') as f:
+        metainfo = json.load(f)
+    
+    if not 'assets' in metainfo:
+        response.status_code = status.HTTP_409_CONFLICT
+        return {}
+    
+    if not asset_type in metainfo['assets']:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {}
+    
+    asset_info = metainfo['assets'][asset_type]
+    
+    response.status_code = status.HTTP_200_OK
+
+    return FileResponse(asset_info.path, filename=os.path.basename(asset_info.identifier), media_type='application/octet-stream')
