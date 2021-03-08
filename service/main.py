d65186110d0921833228b6a98464662d35200e77
Willy Scheibel <willy.scheibel@hpi.de>
2021-03-08 18:11:17 +0100

    Add CORS support

--- service/main.py
+++ service/main.py
@@ -1,6 +1,10 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .v1 import router as v1_router
+
 
 app = FastAPI()
 
@@ -8,6 +12,18 @@ app.include_router(
     v1_router,
     # prefix="/v1",
 )
+
+
+origins = os.environ.get('CORS_ALLOWED_DOMAINS', '').split(';')
+
+if len(origins) > 0:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 if __name__ == "__main__":
